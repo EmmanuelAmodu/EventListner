5840a857f936ddf0f8aa94da538963ce8a7c2bee
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2017-04-18 13:22:14 +0100

    directory example

---
+++ app/js/directives/eventThumbnail.js
@@ -0,0 +1,7 @@
+'use strict';
+eventsApp.directive('mysample', function($compile){
+    return {
+        restrict: 'E',
+        template: "<input type='text' ng-model='sampleData' /> {{sampleData}} <br />"
+    }
+});
---
+++ app/js/directives/mySampleDirective.js
@@ -0,0 +1,8 @@
+'use strict';
+
+eventsApp.directive('mysample', function($compile){
+    return {
+        restrict: 'E',
+        template: "<input type='text' ng-model='sampleData' /> {{sampleData}} <br />"
+    }
+});
